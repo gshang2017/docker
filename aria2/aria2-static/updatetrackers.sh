19faef69c85af222bc0ac88c811b31114d991874
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2018-11-09 09:43:30 +0800

    Update updatetrackers.sh

--- aria2/aria2-static/updatetrackers.sh
+++ aria2/aria2-static/updatetrackers.sh
@@ -10,6 +10,16 @@ if [ $Newtrackers == $Oldtrackers ];then
 echo trackers文件一样,不需要更新。
 else
 sed -i 's@'"$Oldtrackers"'@'"$Newtrackers"'@g'   /usr/local/aria2/aria2.conf 
+#kill aria2
+ps -ef |grep aria2.conf |grep -v grep|awk '{print $1}'|xargs kill -9
+#重启aria2
+if [ ! -n "$RPC_SECRET" ] ;  then 
+aria2c --conf-path=/usr/local/aria2/aria2.conf --enable-rpc --rpc-listen-all   -D
+elif  [ "$SECRETAUTO" == "YES" ]; then
+aria2c --conf-path=/usr/local/aria2/aria2.conf --enable-rpc --rpc-listen-all --rpc-secret="$RPC_SECRET" -D
+else 
+aria2c --conf-path=/usr/local/aria2/aria2.conf --enable-rpc --rpc-listen-all --rpc-secret="$RPC_SECRET" -D
+fi
 echo 已更新trackers。
 fi
 
