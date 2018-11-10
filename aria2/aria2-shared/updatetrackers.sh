09d106a68dea634ecad5ec48e57f40e32beb78b1
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2018-11-10 11:23:54 +0800

    Update updatetrackers.sh

--- aria2/aria2-shared/updatetrackers.sh
+++ aria2/aria2-shared/updatetrackers.sh
@@ -4,12 +4,12 @@ if [ "$TRACKERSAUTO" == "YES" ];then
 
 wget -qP  /tmp  https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt --no-check-certificate 
 Newtrackers="bt-tracker=`awk NF /tmp/trackers_all.txt|sed ":a;N;s/\n/,/g;ta"`"
-Oldtrackers="`grep bt-tracker=  /usr/local/aria2/aria2.conf`" 
+Oldtrackers="`grep bt-tracker=  $CONFIG/aria2.conf`" 
 
 if [ $Newtrackers == $Oldtrackers ];then
 echo trackers文件一样,不需要更新。
 else
-sed -i 's@'"$Oldtrackers"'@'"$Newtrackers"'@g'   /usr/local/aria2/aria2.conf 
+sed -i 's@'"$Oldtrackers"'@'"$Newtrackers"'@g'   $CONFIG/aria2.conf 
 #kill aria2
 ps -ef |grep aria2.conf |grep -v grep|awk '{print $1}'|xargs kill -9
 #重启aria2
