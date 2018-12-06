a973340d72929326af55d974d7399568cef028c0
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2018-12-06 12:02:53 +0800

    Create anki-sync-server.sh

---
+++ anki-sync-server/root/etc/cont-init.d/anki-sync-server.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/with-contenv bash
+
+#检查配置文件位置
+if [ ! -f /config/ankisyncd.conf ] ;  then 
+cp /usr/local/anki-sync-server/defaults/ankisyncd.conf /config
+fi
+if [  -f /usr/local/anki-sync-server/ankisyncd.conf ] ;  then 
+rm  /usr/local/anki-sync-server/ankisyncd.conf
+fi
+if [ ! -L /usr/local/anki-sync-server/ankisyncd.conf ] ;  then 
+ln -s /config/ankisyncd.conf  /usr/local/anki-sync-server/ankisyncd.conf
+fi
+
+
+#添加user.
+if [  -n "$USER" ] && [ -n "$PASSWORD" ] ;  then
+if [ ! -d  /config/collections/$USER ] ;  then
+expect  /usr/local/anki-sync-server/useradd.sh  $USER $PASSWORD
+fi
+fi
