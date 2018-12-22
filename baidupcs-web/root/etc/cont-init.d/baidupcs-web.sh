ceb3233fb9d82056441c2aaf4cf1a9510790a8ee
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2018-12-22 16:26:32 +0800

    Update baidupcs-web.sh

--- baidupcs-web/root/etc/cont-init.d/baidupcs-web.sh
+++ baidupcs-web/root/etc/cont-init.d/baidupcs-web.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/with-contenv bash
 
 #检查配置文件位置
-
-if [ ! -d /config/baidupcs-web ] ;  then 
-mkdir -p /config/baidupcs-web
-fi
 if [ ! -d /root/.config ] ;  then 
 mkdir -p /root/.config
 fi
@@ -12,5 +8,5 @@ if [ -d /root/.config/BaiduPCS-Go ] ;  then
 rm -rf /root/.config/BaiduPCS-Go
 fi
 if [ ! -L /root/.config/BaiduPCS-Go ] ;  then 
-ln -s /config/baidupcs-web   /root/.config/BaiduPCS-Go
+ln -s /config   /root/.config/BaiduPCS-Go
 fi
