f9eafeb8075101cbd6b2e4aa1cb05cb8c5a0f0b0
jsda <jsda@users.noreply.github.com>
2019-01-31 00:55:00 +0800

    Create entrypoint.sh

---
+++ baidupcs-web/entrypoint.sh
@@ -0,0 +1,11 @@
+cd /
+date -R
+curl -s https://api.github.com/repos/liuzhuoling2011/baidupcs-web/releases/latest \
+  | grep browser_download_url \
+  | grep linux-amd64 \
+  | cut -d '"' -f 4 \
+  | wget -qi -
+unzip BaiduPCS-Go-*.zip
+mv BaiduPCS-Go-*/BaiduPCS-Go /usr/local/bin/BaiduPCS-Go
+rm -rf BaiduPCS-Go-**
+chmod a+x /usr/local/bin/BaiduPCS-Go
