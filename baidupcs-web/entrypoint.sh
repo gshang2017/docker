18f20f21b380980235c4f5ab40bcfec08fe368a5
jsda <jsda@users.noreply.github.com>
2019-01-31 03:45:49 +0800

    Update entrypoint.sh

--- baidupcs-web/entrypoint.sh
+++ baidupcs-web/entrypoint.sh
@@ -1,11 +1,12 @@
 cd /
 date -R
-curl -s https://api.github.com/repos/liuzhuoling2011/baidupcs-web/releases/latest \
-  | grep browser_download_url \
-  | grep linux-amd64 \
-  | cut -d '"' -f 4 \
-  | wget -qi - && \
+rm -rf BaiduPCS-Go-* && \
+curl --silent "https://api.github.com/repos/liuzhuoling2011/baidupcs-web/releases/latest" |
+  grep '"tag_name":' |
+  sed -E 's/.*"([^"]+)".*/\1/' |
+  xargs -I {} curl -sOL "https://github.com/liuzhuoling2011/baidupcs-web/releases/download/"{}/BaiduPCS-Go-{}'-linux-amd64.zip'&& \
   unzip BaiduPCS-Go-*.zip && \
+  rm -rf /usr/local/bin/BaiduPCS-Go && \
   mv BaiduPCS-Go-*/BaiduPCS-Go /usr/local/bin/BaiduPCS-Go && \
   rm -rf BaiduPCS-Go-* && \
   chmod a+x /usr/local/bin/BaiduPCS-Go && \
