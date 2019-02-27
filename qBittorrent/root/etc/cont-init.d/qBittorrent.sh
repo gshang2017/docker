a421dc9f870f7753b1c8f0fcb9fed85dbc816d87
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2019-02-27 19:13:51 +0800

    Update qBittorrent.sh

--- qBittorrent/root/etc/cont-init.d/qBittorrent.sh
+++ qBittorrent/root/etc/cont-init.d/qBittorrent.sh
@@ -7,13 +7,10 @@ cp /usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/
 fi
 
 #检查Search文件，并创建.
-if [ ! -d "/config/qBittorrent/Search" ] ;  then 
-cp -rf /usr/local/qbittorrent/defaults/Search  /config/qBittorrent
-fi
 if [ ! -d "/config/qBittorrent/data/nova3/engines" ] ;  then 
 mkdir -p /config/qBittorrent/data/nova3/engines
 fi
-cp -ru /config/qBittorrent/Search/*  /config/qBittorrent/data/nova3/engines
+cp -ru /usr/local/qbittorrent/defaults/Search/*  /config/qBittorrent/data/nova3/engines
 
 
 #设定trackers更新任务
