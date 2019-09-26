5106c68ccd2d6a26c955207d7d2dfd8c629433e3
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2019-09-26 17:43:00 +0800

    Update qiandao.sh

--- qiandao/root/etc/cont-init.d/qiandao.sh
+++ qiandao/root/etc/cont-init.d/qiandao.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/with-contenv bash
 
 #设置配置文件
-if [ "$MAIL_SSL" == "True" ];then
-cp /usr/local/qiandao/mailssl/config.py  /usr/local/qiandao/
-cp /usr/local/qiandao/mailssl/utils.py  /usr/local/qiandao/libs/utils.py
+if [ "$MAIL_STARTTLS" == "True" ];then
+cp /usr/local/qiandao/mailstarttls/config.py  /usr/local/qiandao/
+cp /usr/local/qiandao/mailstarttls/utils.py  /usr/local/qiandao/libs/utils.py
 sed -i 's#./database.db#/dbpath/database.db#'  /usr/local/qiandao/config.py
 sed -i 's/'qiandao.today'/'$DOMAIN'/'  /usr/local/qiandao/config.py
 sed -i 's/mail_smtp = ""/mail_smtp = "'"$MAIL_STMP"'"/'  /usr/local/qiandao/config.py
@@ -21,6 +21,8 @@ cp /usr/local/qiandao/defaults/utils.py  /usr/local/qiandao/libs/utils.py
 sed -i 's#./database.db#/dbpath/database.db#'  /usr/local/qiandao/config.py
 sed -i 's/'qiandao.today'/'$DOMAIN'/'  /usr/local/qiandao/config.py
 sed -i 's/mail_smtp = ""/mail_smtp = "'"$MAIL_STMP"'"/'  /usr/local/qiandao/config.py
+sed -i 's/mail_ssl = True/mail_ssl = '"$MAIL_SSL"'/'  /usr/local/qiandao/config.py
+sed -i 's/mail_port = 465/mail_port = '"$MAIL_PORT"'/'  /usr/local/qiandao/config.py
 sed -i 's/mail_user = ""/mail_user = "'"$MAIL_USER"'"/'  /usr/local/qiandao/config.py
 sed -i 's/mail_password = ""/mail_password = "'"$MAIL_PASSWORD"'"/'  /usr/local/qiandao/config.py
 sed -i 's/mail_domain = "mail.qiandao.today"/mail_domain = "'"$MAIL_DOMAIN"'"/'  /usr/local/qiandao/config.py
