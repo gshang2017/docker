240afbcb996230964850977a25d37b4a3fba7a92
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2019-12-04 15:41:38 +0800

    Update updatetrackers.sh
    fix trackers  Connection refused

--- aria2/root/usr/local/aria2/updatetrackers.sh
+++ aria2/root/usr/local/aria2/updatetrackers.sh
@@ -2,7 +2,8 @@
 
 if [ "$TRACKERSAUTO" == "YES" ];then
 
-wget -qP  /tmp  https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt --no-check-certificate 
+curl -so  /tmp/trackers_all.txt https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt
+#wget -qP  /tmp  https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt --no-check-certificate 
 Newtrackers="bt-tracker=`awk NF /tmp/trackers_all.txt|sed ":a;N;s/\n/,/g;ta"`"
 Oldtrackers="`grep bt-tracker=  /config/aria2.conf`" 
 
