49dc2088659de9a9c714d3c59ad47c3981dcfd6c
gshang2017 <34418201+gshang2017@users.noreply.github.com>
2020-04-25 15:36:09 +0800

    Create sogouscelupdate.sh

---
+++ rime-sogou/root/etc/cont-init.d/sogouscelupdate.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/with-contenv bash
+
+#设定sogouscelupdate更新任务
+if [ `grep  -c sogouscelupdate.py /var/spool/cron/crontabs/root` -eq 0 ];then
+echo "0       */8       *       *       *       python3 /usr/local/sogouscelupdate/sogouscelupdate.py" >> /var/spool/cron/crontabs/root
+echo sogouscelupdate更新任务已设定。
+else
+echo sogouscelupdate更新任务已存在。
+fi
+
+#设置时区
+ln -sf /usr/share/zoneinfo/$TZ   /etc/localtime 
+echo $TZ > /etc/timezone
