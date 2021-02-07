3c77c71c4580c10f7f779c249219fbd6ae242eb7
John <gshang2010@gmail.com>
2021-02-07 10:40:13 +0800

    aria2 update alpine:3.13

--- aria2/root/etc/cont-init.d/aria2c.sh
+++ aria2/root/etc/cont-init.d/aria2c.sh
@@ -3,15 +3,15 @@
 #检查自定义config位置文件
 
 if [ ! -e "/config/aria2.conf" ] ;  then
-cp /usr/local/aria2/defaults/aria2.conf  /config/aria2.conf
+  cp /usr/local/aria2/defaults/aria2.conf  /config/aria2.conf
 fi
 
 if [ ! -e "/config/aria2.session" ] ;  then
-touch /config/aria2.session
+  touch /config/aria2.session
 fi
 
 if [ ! -e "/config/dht.dat" ] ;  then
-touch /config/dht.dat
+  touch /config/dht.dat
 fi
 
 
@@ -24,21 +24,21 @@ sed -i 's/max:16/max:128/g' /usr/local/aria2/AriaNg/js/aria-ng*
 sed -i 's/defaultValue:"20M"/defaultValue:"4k"/g' /usr/local/aria2/AriaNg/js/aria-ng*
 if  [ "$SECRETAUTO" == "YES" ]; then
 #替换js字符串(添加设置的token值为默认。)
-secret=`echo -n  $RPC_SECRET|base64`
-sed -i 's/secret:""/secret:"'"$secret"'"/g'    /usr/local/aria2/AriaNg/js/aria-ng*
+  secret=`echo -n  $RPC_SECRET|base64`
+  sed -i 's/secret:""/secret:"'"$secret"'"/g'    /usr/local/aria2/AriaNg/js/aria-ng*
 fi
 
 #设定trackers更新任务
 if [ `grep  -c updatetrackers.sh /var/spool/cron/crontabs/root` -eq 0 ];then
-echo "0       0       *       *       *       /usr/local/aria2/updatetrackers.sh" >> /var/spool/cron/crontabs/root
-echo trackers更新任务已设定。
+  echo "0       0       *       *       *       /usr/local/aria2/updatetrackers.sh" >> /var/spool/cron/crontabs/root
+  echo trackers更新任务已设定。
 else
-echo trackers更新任务已存在。
+  echo trackers更新任务已存在。
 fi
 
 #启动更新trackers。
 if [ "$TRACKERSAUTO" == "YES" ];then
-/usr/local/aria2/updatetrackers.sh
+  /usr/local/aria2/updatetrackers.sh
 fi
 
 #设置时区
