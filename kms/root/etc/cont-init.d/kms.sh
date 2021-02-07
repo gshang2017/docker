86aff22471e49b16c31dd8d6513e65fde990bf28
John <gshang2010@gmail.com>
2021-02-07 12:57:26 +0800

    kms update alpine:3.13

--- kms/root/etc/cont-init.d/kms.sh
+++ kms/root/etc/cont-init.d/kms.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/with-contenv bash
 
 if [ "$WEB" == "YES" ];then
-cp -rf /etc/darkhttpd   /etc/services.d/
-chmod a+x /etc/services.d/darkhttpd/run
-elif [ -n "/etc/services.d/darkhttpd/run" ] ;  then 
-rm -rf  /etc/services.d/darkhttpd
-
-fi 
+  cp -rf /etc/darkhttpd   /etc/services.d/
+  chmod a+x /etc/services.d/darkhttpd/run
+elif [ -n "/etc/services.d/darkhttpd/run" ] ;  then
+  rm -rf  /etc/services.d/darkhttpd
+fi
