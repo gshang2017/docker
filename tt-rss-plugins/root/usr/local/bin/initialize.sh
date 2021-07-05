9bb168a7fbbd13b78b15f4f94829c6e81e227f05
John <gshang2010@gmail.com>
2021-07-05 15:06:09 +0800

    fix Base database schema is missing

--- tt-rss-plugins/root/usr/local/bin/initialize.sh
+++ tt-rss-plugins/root/usr/local/bin/initialize.sh
@@ -5,9 +5,7 @@ if [  -n "$TTRSS_DB_HOST" ] && [ -n "$TTRSS_DB_USER" ] && [ -n "$TTRSS_DB_NAME" 
     done
     PSQL="psql -q -h $TTRSS_DB_HOST -U $TTRSS_DB_USER $TTRSS_DB_NAME"
     $PSQL -c "create extension if not exists pg_trgm"
-    if ! $PSQL -c 'select * from ttrss_version'; then
-	    $PSQL < /usr/local/tt-rss/schema/ttrss_schema_pgsql.sql
-    fi
+    su - ttrss -s /bin/sh -m -c "php8 /usr/local/tt-rss/update.php --update-schema=force-yes"
 else
     echo 环境变量TTRSS_DB_[HOST、USER、NAME、PASS]未填写完全...
 fi
