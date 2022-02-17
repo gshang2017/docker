a7ce138612f8ee172e2c0b82eafa9f1b9b4e9b92
John <gshang2010@gmail.com>
2022-02-17 16:50:10 +0800

    qBittorrent:4.4.1 qee:4.4.1.10

--- qBittorrent/root/etc/cont-init.d/qBittorrent.sh
+++ qBittorrent/root/etc/cont-init.d/qBittorrent.sh
@@ -1,20 +1,21 @@
 #! /usr/bin/with-contenv bash
 
 #检查config配置文件，并创建.
-if [ ! -e "/config/qBittorrent/config/qBittorrent.conf" ] ;  then
-mkdir -p /config/qBittorrent/config/
-cp /usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/qBittorrent.conf
+if [ ! -d /config/qBittorrent/config ]; then
+  mkdir -p /config/qBittorrent/config
+fi
+if [ ! -e /config/qBittorrent/config/qBittorrent.conf ]; then
+cp /usr/local/qbittorrent/defaults/qBittorrent.conf /config/qBittorrent/config/qBittorrent.conf
 fi
 
 #检查Search文件，并创建.
-if [ ! -d "/config/qBittorrent/data/nova3/engines" ] ;  then
+if [ ! -d /config/qBittorrent/data/nova3/engines ]; then
 mkdir -p /config/qBittorrent/data/nova3/engines
 fi
-cp -ru /usr/local/qbittorrent/defaults/Search/*  /config/qBittorrent/data/nova3/engines
-
+cp -ru /usr/local/qbittorrent/defaults/Search/* /config/qBittorrent/data/nova3/engines
 
 #设定trackers更新任务
-if [ `grep  -c updatetrackers.sh /var/spool/cron/crontabs/root` -eq 0 ];then
+if [ `grep  -c updatetrackers.sh /var/spool/cron/crontabs/root` -eq 0 ]; then
 echo "0       0       *       *       *       /usr/local/qbittorrent/updatetrackers.sh" >> /var/spool/cron/crontabs/root
 echo trackers更新任务已设定。
 else
@@ -22,12 +23,12 @@ echo trackers更新任务已存在。
 fi
 
 #启动时更新trackers。
-if [ "$TRACKERSAUTO" == "YES" ];then
+if [ "$QB_TRACKERS_UPDATE_AUTO" == "true" ]; then
 /usr/local/qbittorrent/updatetrackers.sh
 fi
 
 #设置时区
-ln -sf /usr/share/zoneinfo/$TZ   /etc/localtime
+ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
 #修改用户UID GID
@@ -37,4 +38,3 @@ usermod -o -u "$UID" qbittorrent
 #修复权限
 chown -R qbittorrent:qbittorrent /config
 chown -R qbittorrent:qbittorrent /Downloads
-chown -R qbittorrent:qbittorrent /usr/local/qbittorrent/defaults
