13dfe2a52e0f664ae69acb7612ac4d727c769174
John <gshang2010@gmail.com>
2025-06-29 20:36:04 +0800

    myMPD:22.0.0

--- mpd/root/etc/cont-init.d/10-init-users.sh
+++ mpd/root/etc/cont-init.d/10-init-users.sh
@@ -1,101 +1,111 @@
 #!/bin/sh
+#
+# Initialize Linux users and groups, by (re)writing /etc/passwd, /etc/group and
+# /etc/shadow.
+#
 
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+err() {
+    echo "ERROR: $*" >&2
+}
+
 user_id_exists() {
-    [ -f /etc/passwd ] && cat /etc/passwd | cut -d':' -f3 | grep -q "^$1\$"
+    [ -f /etc/passwd ] && cut -d':' -f3 < /etc/passwd | grep -q "^$1\$"
 }
 
 user_name_exists() {
-    [ -f /etc/passwd ] && cat /etc/passwd | cut -d':' -f1 | grep -q "^$1\$"
+    [ -f /etc/passwd ] && cut -d':' -f1 < /etc/passwd | grep -q "^$1\$"
 }
 
 group_id_exists() {
-    [ -f /etc/group ] && cat /etc/group | cut -d':' -f3 | grep -q "^$1\$"
+    [ -f /etc/group ] && cut -d':' -f3 < /etc/group | grep -q "^$1\$"
 }
 
 group_name_exists() {
-    [ -f /etc/group ] && cat /etc/group | cut -d':' -f1 | grep -q "^$1\$"
+    [ -f /etc/group ] && cut -d':' -f1 < /etc/group | grep -q "^$1\$"
 }
 
 get_group_name_from_group_id() {
     if [ -f /etc/group ]; then
-        cat /etc/group | grep ":x:$1:" | head -n1 | cut -d':' -f1
+        grep ":x:$1:" /etc/group | head -n1 | cut -d':' -f1
     fi
 }
 
 add_group() {
-    DUPLICATE_CHECK=true
+    duplicate_check=true
     if [ "$1" = "--allow-duplicate" ]; then
-        DUPLICATE_CHECK=false
+        duplicate_check=false
         shift
     fi
 
-    NAME="$1"
-    GID="$2"
+    name="$1"
+    gid="$2"
 
-    if $DUPLICATE_CHECK && group_id_exists "$GID"; then
-        echo "ERROR: group ID '$GID' already exists."
+    if ${duplicate_check} && group_id_exists "${gid}"; then
+        err "group ID '${gid}' already exists."
         return 1
-    elif $DUPLICATE_CHECK && group_name_exists "$NAME"; then
-        echo "ERROR: group '$NAME' already exists."
+    elif ${duplicate_check} && group_name_exists "${name}"; then
+        err "group '${name}' already exists."
         return 1
     fi
 
-    echo "$NAME:x:$GID:" >> /etc/group
+    echo "${name}:x:${gid}:" >> /etc/group
 }
 
 add_user() {
-    DUPLICATE_CHECK=true
+    duplicate_check=true
     if [ "$1" = "--allow-duplicate" ]; then
-        DUPLICATE_CHECK=false
+        duplicate_check=false
         shift
     fi
 
-    NAME="$1"
-    UID="$2"
-    GID="$3"
-    HOMEDIR="${4:-/dev/null}"
+    name="$1"
+    uid="$2"
+    gid="$3"
+    homedir="${4:-/dev/null}"
 
-    if $DUPLICATE_CHECK && user_id_exists "$UID"; then
-        echo "ERROR: user ID '$UID' already exists."
+    if ${duplicate_check} && user_id_exists "${uid}"; then
+        err "user ID '${uid}' already exists."
         return 1
-    elif $DUPLICATE_CHECK && user_name_exists "$NAME"; then
-        echo "ERROR: user '$NAME' already exists."
+    elif ${duplicate_check} && user_name_exists "${name}"; then
+        err "user '${name}' already exists."
         return 1
-    elif ! group_id_exists "$GID"; then
-        echo "ERROR: group ID '$GID' doesn't exist."
+    elif ! group_id_exists "${gid}"; then
+        err "group ID '${gid}' doesn't exist."
         return 1
     fi
 
     # Add the user to '/etc/passwd'.
-    echo "$NAME::$UID:$GID::$HOMEDIR:/sbin/nologin" >> /etc/passwd
+    echo "${name}::${uid}:${gid}::${homedir}:/sbin/nologin" >> /etc/passwd
 
     # Add a corresponding entry to '/etc/shadow'.
-    echo "$NAME:!::0:::::" >> /etc/shadow
+    echo "${name}:!::0:::::" >> /etc/shadow
 }
 
 add_user_to_group() {
-    UNAME="$1"
-    GNAME="$2"
+    uname="$1"
+    gname="$2"
 
-    if ! user_name_exists "$UNAME"; then
-        echo "ERROR: user '$UNAME' doesn't exists."
+    if ! user_name_exists "${uname}"; then
+        err "user '${uname}' doesn't exists."
         exit 1
-    elif ! group_name_exists "$GNAME"; then
-        echo "ERROR: group '$GNAME' doesn't exists."
+    elif ! group_name_exists "${gname}"; then
+        err "group '${gname}' doesn't exists."
         exit 1
     fi
 
-    if cat /etc/group | grep -q "^$GNAME:.*:$"; then
-        sed-patch "/^$GNAME:/ s/$/$UNAME/" /etc/group
+    if grep -q "^${gname}:.*:$" /etc/group; then
+        sed-patch "/^${gname}:/ s/$/${uname}/" /etc/group
     else
-        sed-patch "/^$GNAME:/ s/$/,$UNAME/" /etc/group
+        sed-patch "/^${gname}:/ s/$/,${uname}/" /etc/group
     fi
 }
 
 # Initialize files.
+#rm -f /etc/passwd /etc/group /etc/shadow
+#touch /etc/passwd /etc/group /etc/shadow
 cp -f /etc/bak.passwd /etc/passwd
 cp -f /etc/bak.group /etc/group
 cp -f /etc/bak.shadow /etc/shadow
@@ -108,46 +118,54 @@ cp -f /etc/bak.shadow /etc/shadow
 # Add the 'shadow' group.
 #add_group shadow 42
 
+# Add the 'cinit' group.
+add_group cinit 72
+
 # Ubuntu and debian require additional user/group for proper packages
 # installation.
 . /etc/os-release
-case "$ID" in
-    ubuntu|debian)
+case "${ID}" in
+    ubuntu | debian)
         # Add the 'staff' group.
         add_group staff 52
 
         # Add the 'nogroup' group.
-        add_group nogroup  65534
+        add_group nogroup 65534
 
         # Add the '_apt' user.
         add_user _apt 105 65534 /nonexistent
         ;;
-    *) break ;;
+    *) ;;
 esac
 
 # Add the 'app' user.
 # NOTE: This user requires special handling, since its user/group ID is
 #       configurable and may match an existing one.
-add_group --allow-duplicate app "$GROUP_ID"
-add_user --allow-duplicate app "$USER_ID" "$GROUP_ID"
+add_group --allow-duplicate app "${GROUP_ID}"
+add_user --allow-duplicate app "${USER_ID}" "${GROUP_ID}"
 add_user_to_group app app
 
 # Handle supplementary groups of user 'app'.
-echo ${SUP_GROUP_IDS:-},${SUP_GROUP_IDS_INTERNAL:-} | tr ',' '\n' | grep -v '^$' | grep -v '^0$' | grep -vw "$GROUP_ID" | sort -nub | while read GID
-do
-    case "$GID" in
-        (*[!0-9]*)
-            echo "ERROR: SUP_GROUP_IDS contains invalid groupd ID '$GID'."
-            exit 1;
-            ;;
-    esac
-    if ! group_id_exists "$GID"; then
-        add_group "grp$GID" "$GID"
-        add_user_to_group app "grp$GID"
-    else
-        add_user_to_group app "$(get_group_name_from_group_id "$GID")"
-    fi
-done
+echo "${SUP_GROUP_IDS:-},${SUP_GROUP_IDS_INTERNAL:-}" \
+    | tr ',' '\n' \
+    | grep -v '^$' \
+    | grep -v '^0$' \
+    | grep -vw "${GROUP_ID}" \
+    | sort -nub \
+    | while read -r gid; do
+        case "${gid}" in
+            '' | *[!0-9]*)
+                err "SUP_GROUP_IDS contains invalid groupd ID '${gid}'."
+                exit 1
+                ;;
+        esac
+        if ! group_id_exists "${gid}"; then
+            add_group "grp${gid}" "${gid}"
+            add_user_to_group app "grp${gid}"
+        else
+            add_user_to_group app "$(get_group_name_from_group_id "${gid}")"
+        fi
+    done
 
 # Finally, set correct permissions on files.
 chmod 644 /etc/passwd
